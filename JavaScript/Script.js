71ea1f77b6a8c9f1b79755148f27a5c8a93ae61f
unknown <ighorvieiraaccain@gmail.com>
2025-02-16 23:45:17 -0300

    ADD de mais duas noticias itau and airbus

---
+++ JavaScript/Script.js
@@ -0,0 +1,120 @@
+const modalopen1=document.getElementById('first-modal')
+const modalopen2=document.getElementById('secund-modal')
+const modalopen3=document.getElementById('trhe-modal')
+const modalopen4=document.getElementById('four-modal')
+const modalopen5=document.getElementById('five-modal')
+const modalopen6=document.getElementById('six-modal')
+const modalopen7=document.getElementById('seven-modal')
+
+function abrirModal1() {
+    modalopen1.showModal()
+}function fecharModal1() {
+    modalopen1.close()
+}
+
+function abrirModal2() {
+    modalopen2.showModal()
+}function fecharModal2() {
+    modalopen2.close()
+}
+
+function abrirModal3() {
+    modalopen3.showModal()
+}function fecharModal3() {
+    modalopen3.close()
+}
+
+function abrirModal4() {
+    modalopen4.showModal()
+}function fecharModal4() {
+    modalopen4.close()
+}
+
+function abrirModal5() {
+    modalopen5.showModal()
+}function fecharModal5() {
+    modalopen5.close()
+}
+
+function abrirModal6() {
+    modalopen6.showModal()
+}function fecharModal6(){
+    modalopen6.close()
+}
+
+function abrirModal7() {
+    modalopen7.showModal()
+}function fecharModal7(){
+    modalopen7.close()
+}
+
+
+function analiselikeplanet() {
+    let like=document.getElementById('btn-deslike-planet')
+    like.remove()
+}
+
+function analisedeslikeplanet() {
+    let deslike=document.getElementById('btn-like-planet')
+    deslike.remove()
+}
+
+function analiselikecofee() {
+    let like=document.getElementById('btn-deslike-cofee')
+    like.remove()
+}
+
+function analisedeslikecofee() {
+    let deslike=document.getElementById('btn-like-cofee')
+    deslike.remove()
+}
+
+function analiselikeBYD() {
+    let like=document.getElementById('btn-deslike-BYD')
+    like.remove()
+}
+
+function analisedeslikeBYD() {
+    let deslike=document.getElementById('btn-like-BYD')
+    deslike.remove()
+}
+
+function analiselikeatris() {
+    let like=document.getElementById('btn-deslike-atris')
+    like.remove()
+}
+
+function analisedeslikeatris() {
+    let deslike=document.getElementById('btn-like-atris')
+    deslike.remove()
+}
+
+function analiselikeilhabela() {
+    let like=document.getElementById('btn-deslike-ilhabela')
+    like.remove()
+}
+
+function analisedeslikeilhabela() {
+    let deslike=document.getElementById('btn-like-ilhabela')
+    deslike.remove()
+}
+
+function analiselikeairbus(){
+    let like=document.getElementById('btn-deslike-airbus');
+    like.remove()
+}
+
+function analisedeslikeairbus(){
+    let deslike=document.getElementById('btn-like-airbus')
+    deslike.remove()
+}
+
+function analiselikeitau(){
+    let like=document.getElementById('btn-deslike-itau');
+    like.remove()
+}
+
+function analisedeslikeitau(){
+    let deslike=document.getElementById('btn-like-itau')
+    deslike.remove()
+}
